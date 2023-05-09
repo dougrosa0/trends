f3ce13dc5df98c5037f482e762fe563809f2b650
Douglas Rosa <drosa@explorica.com>
2023-05-09 14:23:57 -0600

    Update write lambda with promise to support async call

--- google-trends-write/write-google-trends-lambda.js
+++ google-trends-write/write-google-trends-lambda.js
@@ -1,72 +1,51 @@
 const googleTrends = require('google-trends-api');
-
-// Create the DynamoDB service client module using ES6 syntax.
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-// Set the AWS Region.
-const REGION = "us-west-2"; // For example, "us-east-1".
-// Create an Amazon DynamoDB service client object.
-export const ddbClient = new DynamoDBClient({ region: REGION });
-
-
-
 const aws = require('aws-sdk');
-aws.config.update({
-    region: 'us-west-2'
-});
-const dynamodb = new aws.DynamoDB();
+const dynamodb = new aws.DynamoDB.DocumentClient();
 
 exports.handler = async function(event) {
-  return googleTrends.dailyTrends({
-	  geo: 'US'
-	}, function(err, results) {
-	  if (err) {
-		console.log(err);
-	  } else {
-		const dailyGoogleTrends = JSON.parse(results);
-		var days = dailyGoogleTrends.default.trendingSearchesDays;
+  try {
+    const results = await googleTrends.dailyTrends({ geo: 'US' });
+    const dailyGoogleTrends = JSON.parse(results);
+    const days = dailyGoogleTrends.default.trendingSearchesDays;
 
-		for (var i = 0; i < days.length; i++) {
-			var day = days[i];
-			var trendingSearches = day.trendingSearches;
-			var searchDate = day.date;
+    for (let i = 0; i < days.length; i++) {
+      const day = days[i];
+      const trendingSearches = day.trendingSearches;
+      const searchDate = day.date;
 
-			for (var j = 0; j < trendingSearches.length; j++) {
-				var search = trendingSearches[j];
-				var rank = j+1;
-				var query = search.title.query;
-				var trafficAmount = search.formattedTraffic;
-				console.log(searchDate + " " + query + " " + trafficAmount);
-				saveItem(query, searchDate, trafficAmount, rank);
-			}
-		}
-	  }
-	});
-}
+      for (let j = 0; j < trendingSearches.length; j++) {
+        const search = trendingSearches[j];
+        const rank = j+1;
+        const query = search.title.query;
+        const trafficAmount = search.formattedTraffic;
+        console.log(searchDate + " " + query + " " + trafficAmount);
+        await saveItem(query, searchDate, trafficAmount, rank);
+      }
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Items saved to DynamoDB" })
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" })
+    };
+  }
+};
 
 function saveItem(queryString, searchDate, trafficAmount, dayRank) {
-	var queryLink = encodeURI('https://google.com/search?q=' + queryString);
-    var params = {
-        Item: {
-			      "search-date": {
-                S: searchDate
-            },
-            "query-text": {
-                S: queryString
-            },
-            "traffic-amount": {
-                S: trafficAmount
-            },
-            "day-rank": {
-                N: dayRank.toString()
-            },
-      			"query-link": {
-      				S: queryLink
-      			}
-        },
-        TableName: "google-trends"
-    };
-    return dynamodb.putItem(params, function(err, data) {
-        if (err) console.log(err, err.stack)
-		else console.log(data);
-    });
+  const queryLink = encodeURI('https://google.com/search?q=' + queryString);
+  const params = {
+    Item: {
+      "searchDate": searchDate,
+      "queryText": queryString,
+      "trafficAmount": trafficAmount,
+      "dayRank": dayRank,
+      "queryLink": queryLink
+    },
+    TableName: "googleTrends"
+  };
+  return dynamodb.put(params).promise();
 }
