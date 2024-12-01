217c761bcf3886c1217dfc44e847e173317de37e
dougrosa0 <d.rosa5@yahoo.com>
2024-12-01 11:07:21 -0700

    add email option to express app

--- trends-express-server/src/controllers/google-trends.js
+++ trends-express-server/src/controllers/google-trends.js
@@ -6,6 +6,85 @@ aws.config.update({
 });
 const docClient = new aws.DynamoDB.DocumentClient();
 
+exports.emailTrends = async function(req) {
+  console.log(req.body);
+  var searchDate = req.body.date;
+  var email = req.body.email
+  console.log("Emailing trends for " + searchDate);
+
+  var params = {
+      TableName : "googleTrends",
+      KeyConditionExpression: "#sd = :searchDate",
+      ExpressionAttributeNames:{
+          "#sd": "searchDate"
+      },
+      ExpressionAttributeValues: {
+          ":searchDate": searchDate
+      }
+  };
+
+  docClient.query(params, function(err, data) {
+      if (err) {
+          console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+      } else {
+          var emailContent = "<h2>Trending Google Searches " + searchDate + "</h2>";
+          emailContent += "<table><tr><th>Rank</th><th>Search</th><th>Traffic amount</th></tr>";
+          console.log("Query succeeded.");
+          console.log("Query text | Day rank | Traffic amount | Query link")
+          data.Items.sort(function(a, b) {
+              return a.dayRank - b.dayRank;
+          })
+          data.Items.forEach(function(item) {
+              emailContent += "<tr><td>" + item.dayRank + "</td><td><a href=\"" + encodeURI('https://google.com/search?q=' + item.queryText) + "\">" + item.queryText + "</a></td><td>" + item.trafficAmount + "</td></tr>";
+              console.log(item.queryText + " | " + item.dayRank + " | " + item.trafficAmount + " | " + item.queryLink);
+          });
+          emailContent += "</table>";
+          sendEmail(emailContent);
+      }
+  });
+
+  function sendEmail(content) {
+
+    // Create sendEmail params data
+    var params = {
+      Destination: {
+        ToAddresses: [
+          email
+        ]
+      },
+      Message: {
+        Body: {
+          Html: {
+          Charset: "UTF-8",
+          Data: content
+          },
+          Text: {
+          Charset: "UTF-8",
+          Data: content
+          }
+        },
+        Subject: {
+          Charset: 'UTF-8',
+          Data: 'Google Trends'
+        }
+        },
+      Source: 'dougrosa0@gmail.com'
+    };
+
+    // Create the promise and SES service object
+    var sendPromise = new aws.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+
+    // Handle promise's fulfilled/rejected states
+    sendPromise.then(
+      function(data) {
+        console.log("Email sent successfully. MessageId: " + data.MessageId);
+      }).catch(
+        function(err) {
+        console.error(err, err.stack);
+      });
+  }
+};
+
 exports.readTrends = async function(req) {
   var searchDate = req.params.date;
 
--- trends-express-server/src/routes/trends-routes.js
+++ trends-express-server/src/routes/trends-routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 const googleTrendsController = require('../controllers/google-trends');
+
+var jsonParser = bodyParser.json()
 
 router.get('/:date', async function (req, res) {
     res.send(await googleTrendsController.readTrends(req));
@@ -8,6 +11,10 @@ router.get('/:date', async function (req, res) {
 
 router.put('/', async function (req, res) {
     res.send(await googleTrendsController.writeTrends());
+});
+
+router.put('/email', jsonParser, async function (req, res) {
+    res.send(await googleTrendsController.emailTrends(req));
 });
 
 module.exports = router;
